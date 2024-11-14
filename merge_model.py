b8f9f315889135a0c026f1273ef4779fa938be36
Adansonia <105221020+ChanJinYeon@users.noreply.github.com>
2024-11-14 16:18:30 +0900

    final

--- merge_model.py
+++ merge_model.py
@@ -1,5 +1,4 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
-from transformers import BitsAndBytesConfig
 from peft import PeftModel
 import torch
 
@@ -8,17 +7,9 @@ adapter_path = "./adapter"
 output_path = "./merged_model"
 model_name = 'Saxo/Linkbricks-Horizon-AI-Korean-Advanced-8B'
 
-nf4_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_use_double_quant=True,
-    bnb_4bit_compute_dtype=torch.bfloat16
-    )
-
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
-    cache_dir=cache_dir,
-    quantization_config=nf4_config
+    cache_dir=cache_dir
     )
 
 tokenizer = AutoTokenizer.from_pretrained(
