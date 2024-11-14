42fb93d9560619186cfc602473da27bdf80d0654
Adansonia <105221020+ChanJinYeon@users.noreply.github.com>
2024-11-14 16:07:24 +0900

    required CUDA

--- merge_model.py
+++ merge_model.py
@@ -1,21 +1,33 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import BitsAndBytesConfig
 from peft import PeftModel
 import torch
 
-def save_merged_model_as_gguf(base_model_path, lora_adapter_path, output_path, cache_dir):
-    model = AutoModelForCausalLM.from_pretrained(base_model_path, torch_dtype=torch.float16, cache_dir=cache_dir)
-    model = PeftModel.from_pretrained(model, lora_adapter_path, cache_dir=cache_dir)
-    tokenizer = AutoTokenizer.from_pretrained(base_model_path, cache_dir=cache_dir)
-    
-    model = model.merge_and_unload()
+cache_dir='./cache'
+adapter_path = "./adapter"
+output_path = "./merged_model"
+model_name = 'Saxo/Linkbricks-Horizon-AI-Korean-Advanced-8B'
 
-    model.save_pretrained(output_path)
-    tokenizer.save_pretrained(output_path)
+nf4_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=torch.bfloat16
+    )
 
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    cache_dir=cache_dir,
+    quantization_config=nf4_config
+    )
 
-if __name__ == "__main__":
-    model_name = 'Saxo/Linkbricks-Horizon-AI-Korean-Advanced-8B'
-    lora_adapter_path = 'adapter'   # adapter 경로
-    output_path = './merged_model'
-    cache_dir = './cache'
-    save_merged_model_as_gguf(model_name, lora_adapter_path, output_path, cache_dir=cache_dir)
+tokenizer = AutoTokenizer.from_pretrained(
+    model_name,
+    cache_dir=cache_dir
+    )
+
+model = PeftModel.from_pretrained(model, adapter_path)
+model = model.merge_and_unload()
+
+model.save_pretrained(output_path)
+tokenizer.save_pretrained(output_path)
