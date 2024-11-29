1fcd6ae8cad0effebedd3bdcf00aef29a9d6e875
Adansonia <105221020+ChanJinYeon@users.noreply.github.com>
2024-11-29 17:42:16 +0900

    Update merge_model.py

--- merge_model.py
+++ merge_model.py
@@ -1,26 +1,23 @@
+import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from peft import PeftModel
-import torch
-
-cache_dir='./cache'
-adapter_path = "./adapter"
-output_path = "./merged_model"
-model_name = 'Saxo/Linkbricks-Horizon-AI-Korean-Advanced-8B'
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+base_model_name_or_path = "Saxo/Linkbricks-Horizon-AI-Korean-Advanced-8B"
+lora_model_path = "./1_3"
+cache_dir="./cache"
 
 model = AutoModelForCausalLM.from_pretrained(
-    model_name,
-    cache_dir=cache_dir
-    ).to(device)
+    base_model_name_or_path,
+    # load_in_4bit=True,    -> kernel died
+    torch_dtype=torch.bfloat16,
+	cache_dir=cache_dir,
+    device_map="cuda"
+)
 
-tokenizer = AutoTokenizer.from_pretrained(
-    model_name,
-    cache_dir=cache_dir
-    )
+tokenizer = AutoTokenizer.from_pretrained(base_model_name_or_path)
+model = PeftModel.from_pretrained(model, lora_model_path)
 
-model = PeftModel.from_pretrained(model, adapter_path).to(device)
 model = model.merge_and_unload()
+save_directory = "./merged_model"
+model.save_pretrained(save_directory)
+tokenizer.save_pretrained(save_directory)
 
-model.save_pretrained(output_path)
-tokenizer.save_pretrained(output_path)
